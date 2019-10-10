f78cf4a7d9e60c50af3419956c749d49f6042fdf
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-11 01:40:48 +0900

    Update score_disp.js

--- score_disp.js
+++ score_disp.js
@@ -1,2 +1,13 @@
-var wrapper=document.getElementById("wrapper");
-wrapper.insertAdjacentHTML('beforeend',"Hello World");
+var xmlHttp=new XMLHttpRequest();
+var url="https://cdn.jsdelivr.net/gh/KazuDon-0407/groove_coaster_score_bookmarklet/genre_sort_database.csv";
+/*id set*/
+xmlHttp.open("GET",url,false);
+xmlHttp.send();
+var arr = xmlHttp.responseText.split('\n');
+
+var res = [];
+ for(var i = 0; i < arr.length; i++){
+    if(arr[i] == '') break;
+    res[i] = arr[i].split(',');
+}
+console.log(res);
