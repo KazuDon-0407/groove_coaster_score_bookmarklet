904f0edd33a2ca291e1f3bff12d8537c5f63c4e1
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-12 21:38:58 +0900

    Update test.js

--- test.js
+++ test.js
@@ -46,6 +46,64 @@ for(var i=0;i<diff_rank;i++){
 }
 
 
+function get_id(){
+    var xmlHttp=new XMLHttpRequest();
+    /*id set*/
+    xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/music_list.php",false);
+    xmlHttp.send(null);
+
+    var data_s=JSON.parse(xmlHttp.responseText);
+    var play_id=data_s.music_list.map(function(e){
+        return e.music_id;
+    });
+    play_id.sort(
+        function(a,b) {
+          return a - b;
+        }
+    );
+    get_score(play_id);
+}
+
+function get_score(id){
+    var data_array=[];
+    var xhr=[];
+    var disp='<html><head><title>新規タブ</title></head><body><h1>あなたの全スコア</h1>';
+    id.forEach(function(music_id,index){
+        /*score dataは添字music_idに格納*/
+        data_array[music_id]=new Array();
+        xhr[music_id]=new XMLHttpRequest();
+        xhr[music_id].open("GET","https://mypage.groovecoaster.jp/sp/json/music_detail.php?music_id="+music_id,true);
+        xhr[music_id].onreadystatechange = function(){
+            if (xhr[music_id].readyState === 4 && xhr[music_id].status === 200){
+                var data=JSON.parse(xhr[music_id].responseText);
+                const path=[data.music_detail.simple_result_data,data.music_detail.normal_result_data,data.music_detail.hard_result_data,data.music_detail.extra_result_data];
+                data_array[music_id][0]=data.music_detail.music_title;
+                var score_data;
+                var diff_lng=4;
+                if(!data.music_detail.ex_flag) diff_lng--;
+                for(var i=0;i<diff_lng;i++){
+                    (path[i]==null) ? score_data=0 : score_data=path[i].score;
+                    data_array[music_id][i+1]=score_data;
+                }
+                if(index==id.length-1){
+                    console.log(data_array);
+                    var csv_array=get_csv();
+                    console.log(csv_array);
+                    data_search(csv_array,data_array);
+                    score_detail();
+                    disp+='</body></html>'
+                    var nwin=window.open();
+                    //nwin.document.open();
+                    nwin.document.write(disp);
+                    //nwin.document.close();
+                    console.log("err");
+                }
+            }
+        };
+        xhr[music_id].send();
+    });
+}
+
 function get_csv(){
     var xmlHttp=new XMLHttpRequest();
     var csv_url="https://kazudon-0407.github.io/groove_coaster_score_bookmarklet/genre_sort_database.csv";
@@ -217,55 +275,5 @@ function score_detail(){
     disp+='<p>300万:'+god_count[0]+'曲</p>';
     disp+='<p>400万:'+god_count[1]+'曲</p>';
 }
-var xmlHttp=new XMLHttpRequest();
-    /*id set*/
-    xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/music_list.php",false);
-    xmlHttp.send(null);
 
-    var data_s=JSON.parse(xmlHttp.responseText);
-    var id=data_s.music_list.map(function(e){
-        return e.music_id;
-    });
-    id.sort(
-        function(a,b) {
-          return a - b;
-        }
-    );
-
-    var data_array=[];
-    var xhr=[];
-    var disp='<html><head><title>新規タブ</title></head><body><h1>あなたの全スコア</h1>';
-    id.forEach(function(music_id,index){
-        /*score dataは添字music_idに格納*/
-        data_array[music_id]=new Array();
-        xhr[music_id]=new XMLHttpRequest();
-        xhr[music_id].open("GET","https://mypage.groovecoaster.jp/sp/json/music_detail.php?music_id="+music_id,true);
-        xhr[music_id].onreadystatechange = function(){
-            if (xhr[music_id].readyState === 4 && xhr[music_id].status === 200){
-                var data=JSON.parse(xhr[music_id].responseText);
-                const path=[data.music_detail.simple_result_data,data.music_detail.normal_result_data,data.music_detail.hard_result_data,data.music_detail.extra_result_data];
-                data_array[music_id][0]=data.music_detail.music_title;
-                var score_data;
-                var diff_lng=4;
-                if(!data.music_detail.ex_flag) diff_lng--;
-                for(var i=0;i<diff_lng;i++){
-                    (path[i]==null) ? score_data=0 : score_data=path[i].score;
-                    data_array[music_id][i+1]=score_data;
-                }
-                if(index==id.length-1){
-                    console.log(data_array);
-                    var csv_array=get_csv();
-                    console.log(csv_array);
-                    data_search(csv_array,data_array);
-                    score_detail();
-                    disp+='</body></html>'
-                    var nwin=window.open();
-                    //nwin.document.open();
-                    nwin.document.write(disp);
-                    //nwin.document.close();
-                    console.log("err");
-                }
-            }
-        };
-        xhr[music_id].send();
-    });
+get_id();
