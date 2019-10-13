f68f6790bf63e665cb8244ef411664d0e842ed89
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 03:35:32 +0900

    Update testtest.js

--- testtest.js
+++ testtest.js
@@ -288,5 +288,4 @@ var tag='<html><head></head><body></body></html>';
 var header='<title>スコア</title><script>function copyToClipboard(){var copyTarget = document.getElementById("CopyTarget");var text = document.createElement("textarea");text.value = copyTarget.innerText;document.body.appendChild(text);text.select();document.execCommand("copy");alert("クリップボードにコピーしました。");text.parentElement.removeChild(text);}</script>';
 document.write(tag);
 //get_id();
-document.write();
-//get_id();
+alert("?");
