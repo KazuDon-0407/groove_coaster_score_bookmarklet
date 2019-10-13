354bdfcb6c31482d24d3a1a14bd8873c9a02f5e6
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 03:55:12 +0900

    Update testtest.js

--- testtest.js
+++ testtest.js
@@ -76,8 +76,7 @@ function get_id(){
 function get_score(id){
     var data_array=[];
     var xhr=[];
-    //disp+='<html><head><title>コピー</title></head><body><h1>あなたの全スコア</h1>';
-    disp+='<h1>あなたの全スコア</h1>';
+    disp+='<html><head><title>スコア</title><script>function copyToClipboard(){var copyTarget = document.getElementById("CopyTarget");var text = document.createElement("textarea");text.value = copyTarget.innerText;document.body.appendChild(text);text.select();document.execCommand("copy");alert("クリップボードにコピーしました。");text.parentElement.removeChild(text);}</script></head><body><h1>あなたの全スコア</h1>';
     id.forEach(function(music_id,index){
         /*score dataは添字music_idに格納*/
         data_array[music_id]=new Array();
@@ -280,14 +279,8 @@ function score_detail(){
 }
 
 function score_disp(){
-    //disp+='</body></html>';
-    document.body.innerHTML=disp;
+    disp+='</body></html>';
+    document.write(disp);
 
 }
-var p=window.open();
-var tag="";
-tag+='<html><head><title>api</title></head><body><p>now loading...</p></body></html>';
-var header='<title>スコア</title><script>function copyToClipboard(){var copyTarget = document.getElementById("CopyTarget");var text = document.createElement("textarea");text.value = copyTarget.innerText;document.body.appendChild(text);text.select();document.execCommand("copy");alert("クリップボードにコピーしました。");text.parentElement.removeChild(text);}</script>';
-p.document.write(tag);
-//get_id();
-alert("?");
+get_id();
