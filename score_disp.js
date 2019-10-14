4a42b41a7fdde98cb943513d59c59c9eb9402f99
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 16:43:10 +0900

    Update score_disp.js

--- score_disp.js
+++ score_disp.js
@@ -94,7 +94,7 @@ function get_id(){
                     return a - b;
                 }
             );
-            alert("OKを押すとデータ取り込みを開始します");
+            alert("OK or 閉じるを押すとデータ取り込みを開始します");
             get_score(play_id);
         }
         else if (xmlHttp.readyState === 4 && xmlHttp.status === 0){
